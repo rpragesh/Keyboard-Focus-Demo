07f7fcb2a93ed7ad1d18aca2690fde12f3a3933e
Ragesh Pikalmunde <ragesh.pikalmunde@decos.com>
2020-04-27 15:19:09 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,7 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
+    Keyboard,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component, ViewChild } from '@angular/core';
+import { Keyboard } from '@ionic-native/keyboard/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  @ViewChild('focusInput', { static: false }) myInput;
+  addressDetails: string;
 
-  constructor() {}
+  constructor(private nativeKeyboard: Keyboard) { }
 
+  ionViewWillEnter() {
+    setTimeout(() => {
+      this.nativeKeyboard.show();
+      this.myInput.setFocus();
+    }, 800);
+  }
 }
